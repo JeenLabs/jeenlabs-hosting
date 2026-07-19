40ab2a2bc1d38933060e814863e1e38a64ad64e4
JeenLabs <247867139+JeenLabs-official@users.noreply.github.com>
2026-07-19 12:43:23 +0530

    chore(repo): allow deps and security commit types
    Extend commitlint type enum to match project Conventional Commit
    rules so dependency and security commits are accepted by hooks.
    
    Signed-off-by: JeenLabs <247867139+JeenLabs-official@users.noreply.github.com>

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,25 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'refactor',
+        'docs',
+        'chore',
+        'build',
+        'deps',
+        'ci',
+        'test',
+        'perf',
+        'style',
+        'security',
+        'revert',
+      ],
+    ],
     'scope-empty': [1, 'never'],
     'body-leading-blank': [2, 'always'],
     'footer-leading-blank': [2, 'always'],
